6c7f43220817a70dfb37c5d58f896a46b09a2824
ywfuping <ywfuping@163.com>
2016-06-30 22:34:41 +0800

    AOP 动态代理模式

--- src/main/java/com/yawei/aop/MyAdvice.java
+++ src/main/java/com/yawei/aop/MyAdvice.java
@@ -1,10 +1,46 @@
 package com.yawei.aop;
 
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class MyAdvice {
 
     //前置通知
-    public void beforeAdvice(){
+    public void beforeAdvice() {
+        System.out.println("前置通知~~~");
+    }
 
+    //前置通知
+    public void afAdvice() {
+        System.out.println("后置通知~~~");
+    }
+
+    public void afAdvice(Object result) {
+        System.out.println("后置通知~~~" + result);
+    }
+
+    //异常通知
+    public void exceptionAdvice(Exception e) {
+        System.out.println("异常通知~~~" + e);
+    }
+
+    //最终通知
+    public void finalAdvice() {
+        System.out.println("最终通知~~~");
+    }
+
+    public Object aroundAdvice(ProceedingJoinPoint joinPoint){
+        Object obj=null;
+        try {
+            System.out.println("前置通知~~~~~~");
+            obj = joinPoint.proceed();
+            System.out.println("后置通知~~~~~~");
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+            System.out.println("异常通知~~~~~~"+throwable);
+        }finally {
+            System.out.println("最终通知~~~~~~");
+        }
+        return obj;
     }
 }
--- src/test/java/com/yawei/test/UserTestCase.java
+++ src/test/java/com/yawei/test/UserTestCase.java
@@ -1,6 +1,7 @@
 package com.yawei.test;
 
 import com.yawei.dao.UserDao;
+import com.yawei.service.BookService;
 import com.yawei.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -11,8 +12,11 @@ public class UserTestCase {
     public void testUserDao(){
         ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
-        UserService userService = (UserService) context.getBean("userService");
-        userService.sayHi();
+        BookService bookService = (BookService) context.getBean("bookService");
+        bookService.showBook();
+
+       /* UserService userService = (UserService) context.getBean("userService");
+        userService.sayHi();*/
 
         /*UserDao userDao = (UserDao) context.getBean("userDao");
         userDao.sayHello();*/
